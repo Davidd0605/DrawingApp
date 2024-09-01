136b93b3487a01422de5a9abfe1051d88b7494d1
David-Gabriel Dediu <135978285+Davidd0605@users.noreply.github.com>
2024-09-01 20:26:46 +0200

    polished the size adjustments

--- inex.js
+++ inex.js
@@ -1,6 +1,9 @@
 let size = 3;
+let colorPicker = document.getElementById("colorPicker")
+let sizeEdit = document.getElementById("gridSize")
 let resetBtn = document.getElementById("reset")
 let container = document.getElementById("container")
+let color = "white"
 function populate()
 {
     let i = 0;
@@ -8,16 +11,32 @@ function populate()
         let cell = document.createElement("div")
         cell.classList.add("cell")
         container.appendChild(cell)
-        cell.addEventListener("click", function(){cell.style.setProperty("background-color", "white")})
-        cell.addEventListener("mouseover", () => {if(draw)
-            cell.style.setProperty("background-color", "white")
-        })
+        
+        cell.addEventListener("click", function(){cell.style.setProperty("background-color", color)})
+        cell.addEventListener("mouseover", () => {
+            if(draw)
+                cell.style.setProperty("background-color", color)})
         i++;
+        console.log(i)
         if(i == size*size)
+        {
             window.clearInterval(intervalId)
-    }, 250)
+        }
+
+    }, 10)
 
 }
+colorPicker.addEventListener("input", ()=>{color = colorPicker.value})
+sizeEdit.addEventListener("input", function(){
+
+    size = sizeEdit.value;
+    if(size <= 0)
+        size = 1;
+    container.style.setProperty("--size",size)
+
+    container.innerHTML = "";
+    populate()
+})
 let draw = false;
 window.addEventListener("mousedown", ()=> { draw = true})
 window.addEventListener("mouseup", ()=>{draw = false})
