488c1e8e9cda8f1d991c21c1dfcff85137f992cc
David-Gabriel Dediu <135978285+Davidd0605@users.noreply.github.com>
2024-09-01 18:31:34 +0200

    Done some stuffs

--- inex.js
+++ inex.js
@@ -8,12 +8,18 @@ function populate()
         let cell = document.createElement("div")
         cell.classList.add("cell")
         container.appendChild(cell)
+        cell.addEventListener("click", function(){cell.style.setProperty("background-color", "white")})
+        cell.addEventListener("mouseover", () => {if(draw)
+            cell.style.setProperty("background-color", "white")
+        })
         i++;
         if(i == size*size)
             window.clearInterval(intervalId)
     }, 250)
 
 }
-
+let draw = false;
+window.addEventListener("mousedown", ()=> { draw = true})
+window.addEventListener("mouseup", ()=>{draw = false})
 resetBtn.onclick = ()=>{ container.innerHTML = ""; populate()}
 populate()
